30488125f32853f3b150d4dcf8072ebaf9432da6
Massimiliano Ghilardi <massimiliano.ghilardi@gmail.com>
2019-11-23 18:08:18 +0100

    move github.com/cosmos72/gomacro/jit to this repository

---
+++ gomacrojit.go
@@ -0,0 +1,21 @@
+/*
+ * gomacrojit - JIT compiler in Go
+ *
+ * Copyright (C) 2019 Massimiliano Ghilardi
+ *
+ *     This Source Code Form is subject to the terms of the Mozilla Public
+ *     License, v. 2.0. If a copy of the MPL was not distributed with this
+ *     file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ *
+ * gomacrojit.go
+ *
+ *  Created on Nov 23, 2019
+ *      Author Massimiliano Ghilardi
+ */
+
+package gomacrojit
+
+import (
+	_ "github.com/cosmos72/gomacrojit/jit"
+)
