3dd1b2602d6757a1af4864f5cd9da9937bdd085a
max <max@debian-usb>
2021-03-06 12:05:03 +0100

    onejit: add more RegAllocator tests

--- test/test_regallocator.cpp
+++ test/test_regallocator.cpp
@@ -31,10 +31,23 @@
 
 namespace onejit {
 
-void Test::regallocator() {
-  using Reg = RegAllocator::Reg;
-  using Color = RegAllocator::Color;
+using Color = RegAllocator::Color;
+using Reg = RegAllocator::Reg;
 
+static void to_string(String &result, View<Color> colors) {
+  result.clear();
+  Fmt fmt{&result};
+  for (Color color : colors) {
+    fmt << color << ' ';
+  }
+}
+
+static void run_allocator(String &result, RegAllocator &allocator, Color num_colors) {
+  allocator.allocate_regs(num_colors);
+  to_string(result, allocator.get_colors());
+}
+
+void Test::regallocator() {
   enum : size_t { nreg = 10 };
   RegAllocator allocator{nreg};
   Graph &graph = allocator.graph();
@@ -52,13 +65,8 @@ void Test::regallocator() {
   }
   // also connect register 4 with 5
   graph.set(Reg(4), Reg(5), true);
-  allocator.allocate_regs(Color(5));
-
   String result;
-  Fmt fmt{&result};
-  for (Color color : allocator.get_colors()) {
-    fmt << color << ' ';
-  }
+  run_allocator(result, allocator, Color(5));
   Chars expected = "4 3 2 1 0 4 3 2 1 0 ";
   TEST(result, ==, expected);
 
@@ -69,12 +77,28 @@ void Test::regallocator() {
     }
   }
   // and try again
-  allocator.allocate_regs(Color(5));
-  result.clear();
-  for (Color color : allocator.get_colors()) {
-    fmt << color << ' ';
-  }
+  run_allocator(result, allocator, Color(5));
   expected = "1 0 5 4 3 2 1 0 4 3 ";
+  TEST(result, ==, expected);
+
+  // retry with fewer available registers
+  run_allocator(result, allocator, Color(4));
+  expected = "3 2 5 1 0 4 3 2 1 0 ";
+  TEST(result, ==, expected);
+
+  // retry with fewer available registers
+  run_allocator(result, allocator, Color(3));
+  expected = "2 1 6 5 0 4 3 2 1 0 ";
+  TEST(result, ==, expected);
+
+  // retry with fewer available registers
+  run_allocator(result, allocator, Color(2));
+  expected = "1 0 7 6 5 4 3 2 1 0 ";
+  TEST(result, ==, expected);
+
+  // and with more available registers
+  run_allocator(result, allocator, Color(6));
+  expected = "3 2 5 1 0 4 3 2 1 0 ";
   TEST(result, ==, expected);
 }
 
