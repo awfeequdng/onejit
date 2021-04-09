e612b6772433bf9b78dff11e250c6d1a2065dde7
Massimiliano Ghilardi <massimiliano.ghilardi@gmail.com>
2021-04-09 19:11:00 +0200

    go/parser: rename Parser method InitParseDirFile -> InitParseFile

--- go/parser/dir.go
+++ go/parser/dir.go
@@ -22,13 +22,14 @@ import (
 
 type FileOpener = func() (filename string, src io.ReadCloser)
 
-// parse all files in a directory. repeatedly calls InitParseFile() until opener returns nil.
+// parse all files in a directory.
+// repeatedly calls Parser.InitParseFile() until opener returns nil.
 // does NOT clear accumulated errors
 func (p *Parser) InitParseDir(fset *token.FileSet, opener FileOpener, mode Mode) *ast.Dir {
 	dir := &ast.Dir{Atom: ast.Atom{Tok: token.DIR}}
 	var files []*ast.File
 	for {
-		file := p.InitParseDirFile(fset, opener, mode)
+		file := p.InitParseFile(fset, opener, mode)
 		if file == nil {
 			break
 		}
@@ -38,9 +39,10 @@ func (p *Parser) InitParseDir(fset *token.FileSet, opener FileOpener, mode Mode)
 	return dir
 }
 
-// open and parse a single file in a directory. calls in sequence: opener(), Parser.Init(), Parser.ParseFile()
+// open and parse a single file in a directory.
+// calls in sequence: opener(), Parser.Init(), Parser.ParseFile()
 // does NOT clear accumulated errors
-func (p *Parser) InitParseDirFile(fset *token.FileSet, opener FileOpener, mode Mode) *ast.File {
+func (p *Parser) InitParseFile(fset *token.FileSet, opener FileOpener, mode Mode) *ast.File {
 	filename, src := opener()
 	if src == nil {
 		return nil
